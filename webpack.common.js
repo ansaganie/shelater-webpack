afdf12fcb16b2eba1ecc5870d985a685054a0870
Ansagan Islamgaliev <ansaganie@hotmail.com>
2022-05-08 11:28:55 +0600

    Add MiniCssExtractor

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,25 +10,22 @@ module.exports = {
       template: './public/index.html',
     }),
     new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: './public',
-          globOptions: {
-            ignore: [
-                '**/index.html'
-            ]
-        }
-        },
-      ]
-    })
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        test: /\.scss$/i,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
       },
+      {
+        test: /\.html$/i,
+        use: [ 'html-loader' ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
     ]
   }
 };
